8138debc22425a55044d04a500997d8ef2897861
Sindhura Gollamudi <gollamudisindhura1@gmail.com>
2025-12-18 21:02:58 -0500

    created interface in todo.ts and created ThemeContext

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,36 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+type Theme = "light" | "dark";
+
+interface ThemeContextType {
+    theme : Theme
+    toggleTheme: () => void
+}
+
+const ThemeContext = createContext<ThemeContextType>({
+    theme: "light",
+    toggleTheme: () =>{}
+})
+
+export const ThemeProvider = ({ children }:{children: React.ReactNode}) => {
+    const [theme, setTheme] = useState<Theme>(() =>{
+        return (localStorage.getItem("theme") as Theme) || "light"
+    })
+
+    const toggleTheme = () => {
+        setTheme(prev => (prev === "light" ? "dark" : "light"))
+    }
+
+    useEffect(() => {
+        localStorage.setItem("theme", theme)
+    }, [theme])
+
+    return (
+        <ThemeContext.Provider value = {{theme, toggleTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+// eslint-disable-next-line react-refresh/only-export-components
+export const useTheme = () => useContext(ThemeContext)
+
